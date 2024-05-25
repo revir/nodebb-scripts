cab233e5968007725aec897cf707252e92a3cf3e
River <revir.qing@gmail.com>
2024-05-25 22:07:02 +0200

    output to backups dir

--- backup-dir.js
+++ backup-dir.js
@@ -1,35 +1,34 @@
 #! /usr/bin/env node
-var nodeCLI = require('shelljs-nodecli');
-var fs = require('fs-extra');
-var archiver = require('archiver');
-var path = require('path');
-var moment = require('moment');
+var fs = require("fs-extra");
+var archiver = require("archiver");
+var path = require("path");
+var moment = require("moment");
 
-console.log('Preparing to backup NodeBB');
+console.log("Preparing to backup NodeBB");
 var pwd = process.argv[2];
 console.log("Present Working Directory: " + pwd);
 
-if (!fs.existsSync(pwd + '/config.json')) {
-    throw 'ERROR '+pwd+' is not nodebb directory!';
+if (!fs.existsSync(pwd + "/config.json")) {
+  throw "ERROR " + pwd + " is not nodebb directory!";
 }
 
 var timeString = moment().format("YYYY-MM-DD");
-var backupFileName = "nodebb-dir-" + timeString + '.zip';
-var outputFilePath = path.join(pwd, "..", backupFileName);
+var backupFileName = "nodebb-dir-" + timeString + ".zip";
+var outputFilePath = path.join(pwd, "../backups", backupFileName);
 var output = fs.createWriteStream(outputFilePath);
-var archive = archiver('zip');
+var archive = archiver("zip");
 
 console.log("Creating compressed backup file: " + outputFilePath);
 
-archive.on('error', function(err){
-    throw err;
+archive.on("error", function (err) {
+  throw err;
 });
 archive.pipe(output);
-archive.directory(pwd, 'dir/');
+archive.directory(pwd, "dir/");
 archive.finalize();
 
-output.on('close', function() {
-  var len = (archive.pointer() / 1024) / 1024;
-  console.log(len + ' MB');
+output.on("close", function () {
+  var len = archive.pointer() / 1024 / 1024;
+  console.log(len + " MB");
   console.log("Done and cleaned up");
 });
--- backup-mongodb.js
+++ backup-mongodb.js
@@ -1,53 +1,60 @@
 #! /usr/bin/env node
-var nodeCLI = require('shelljs-nodecli');
-var fs = require('fs-extra');
-var archiver = require('archiver');
-var path = require('path');
-var moment = require('moment');
+var nodeCLI = require("shelljs-nodecli");
+var fs = require("fs-extra");
+var path = require("path");
+var moment = require("moment");
 
-console.log('Preparing to backup NodeBB');
+console.log("Preparing to backup NodeBB");
 var pwd = process.argv[2];
 console.log("Present Working Directory: " + pwd);
 
 var dockerName = process.argv[3];
+var dbName = process.argv[4];
 
 //---------------------------------------------
 //Find and load config.json settings for NodeBB, give warning and exit if not Mongo
 //---------------------------------------------
 
+var backupsDir = path.join(pwd, "../backups");
+if (!fs.existsSync(backupsDir)) {
+  fs.mkdirSync(backupsDir);
+}
+console.log("backupsDir: " + backupsDir);
 
 var configPath = path.join(pwd, "/config.json");
 console.log("Loading: " + configPath);
 
 var configContents;
 try {
-  configContents = fs.readFileSync(configPath, 'utf8');
+  configContents = fs.readFileSync(configPath, "utf8");
 } catch (err) {
-    console.log("Unable to load necessary nodebb config.json file: " + configPath);
-    throw err;
+  console.log(
+    "Unable to load necessary nodebb config.json file: " + configPath
+  );
+  throw err;
 }
 
 var config = JSON.parse(configContents);
 if (config.database !== "mongo") {
-  throw new Error("Currently this script only works with backing up mongo.  Want to improve it?  https://github.com/jongarrison/nodebb-backup")
+  throw new Error(
+    "Currently this script only works with backing up mongo.  Want to improve it?  https://github.com/jongarrison/nodebb-backup"
+  );
 }
-
-console.log("Found db config info for: " + config.mongo.database);
-
 
 //---------------------------------------------
 //Find and load current NodeBB version name from package.json (like: 0.7.3)
 //---------------------------------------------
-
 
 var packagePath = path.join(pwd, "/package.json");
 console.log("Loading: " + packagePath);
 
 var packageContents;
 try {
-  packageContents = fs.readFileSync(packagePath, 'utf8');
+  packageContents = fs.readFileSync(packagePath, "utf8");
 } catch (err) {
-  console.log("Unable to load necessary nodebb package.json file: " + packagePath);
+  console.log(
+    "Unable to load necessary nodebb package.json file: " + packagePath
+  );
   throw err;
 }
 
@@ -55,70 +62,34 @@ var package = JSON.parse(packageContents);
 
 console.log("Found NodeBB version: " + package.version);
 
-
-//---------------------------------------------
-//Create temp-backup directory to load back up files into
-//---------------------------------------------
-
-
-var tempBackupDir = path.join(pwd, "/temp-backup");
-console.log("creating temp backup directory at: " + tempBackupDir);
-try {
-  if (fs.existsSync(tempBackupDir)) {
-    //try to clean it up
-    fs.removeSync(tempBackupDir);
-    if (fs.existsSync(tempBackupDir)) { throw new Error("Unable to remove dir"); }
-  }
-} catch (err) {
-  console.log("Unable to continue.  Dirty backup dir already exists at: " + tempBackupDir + "\n" + err);
-  process.exit(1);
-}
-
-fs.mkdirSync(tempBackupDir);
-
 //---------------------------------------------
 //MongoDump into temp-backup directory
 //---------------------------------------------
 
-
-console.log("About to backup db: " + config.mongo.database);
-
-var args = ['-v',
-'-d', config.mongo.database,
-config.mongo.username ? '-u '+config.mongo.username : '',
-config.mongo.password ? '-p '+'"'+config.mongo.password+'"' : '',
-'-o', tempBackupDir,
-'-h', config.mongo.host + ":" + config.mongo.port];
+dbName = dbName || config.mongo.database;
+console.log("About to backup db: " + dbName);
 
 if (dockerName) {
-  nodeCLI.exec('docker', 'exec', '-it', dockerName, 'mongodump', ...args);
-} else {
-  nodeCLI.exec('mongodump', ...args);
+  backupsDir = "/backups";
 }
 
-//---------------------------------------------
-//Now zip them up, give them a reasonable name and move the whole thing up one directory to get out of this git repo
-//---------------------------------------------
-
-
 var timeString = moment().format("YYYY-MM-DD");
-var backupFileName = "nodebb-db-" + config.mongo.database + '-' + timeString + '.zip';
-var outputFilePath = path.join(pwd, "..", backupFileName);
-var output = fs.createWriteStream(outputFilePath);
-var archive = archiver('zip');
+var backupFileName = "db-" + dbName + "-" + timeString + ".gz";
 
-console.log("Creating compressed backup file: " + outputFilePath);
+var args = [
+  "-v",
+  "-d",
+  dbName,
+  config.mongo.username ? "-u " + config.mongo.username : "",
+  config.mongo.password ? "-p " + '"' + config.mongo.password + '"' : "",
+  "-h",
+  config.mongo.host + ":" + config.mongo.port,
+  "--gzip",
+  "--archive=" + path.join(backupsDir, backupFileName),
+];
 
-archive.on('error', function(err){
-    throw err;
-});
-archive.pipe(output);
-archive.directory(tempBackupDir, 'db/');
-archive.finalize();
-
-output.on('close', function() {
-  var len = (archive.pointer() / 1024) / 1024;
-  console.log(len + ' MB');
-  fs.remove(tempBackupDir);
-  console.log("Done and cleaned up");
-});
+if (dockerName) {
+  nodeCLI.exec("docker", "exec", dockerName, "mongodump", ...args);
+} else {
+  nodeCLI.exec("mongodump", ...args);
+}
